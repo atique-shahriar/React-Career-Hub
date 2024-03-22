71a273b3a6cec5dd9b46394cd858e80522111f26
atique-shahriar <satique06@gmail.com>
2024-03-23 00:08:45 +0600

    Create some components

---
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -0,0 +1,11 @@
+
+
+const AppliedJobs = () => {
+    return (
+        <div>
+            Hello
+        </div>
+    );
+};
+
+export default AppliedJobs;
--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,33 @@ import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './components/Home/Home';
 import Root from './components/Root/Root';
+import AppliedJobs from './components/AppliedJobs/AppliedJobs';
+import Blog from './components/Blog/Blog';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: "/",
         element: <Home></Home>
       },
+      {
+        path: "/applied",
+        element: <AppliedJobs></AppliedJobs>
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>
+      }
     ]
   },
 ]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
